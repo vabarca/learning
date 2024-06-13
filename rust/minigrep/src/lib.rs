71e3675b3b218e1eb01395368c0076e2bec00573
vabarca <viabfer@gmail.com>
2024-06-13 20:11:56 +0200

    Update

--- rust/minigrep/src/lib.rs
+++ rust/minigrep/src/lib.rs
@@ -1,6 +1,21 @@
-
-
-pub struct Config{
+pub struct Config {
     pub filename: String,
     pub query: String,
+}
+
+pub fn print_result(result: &Vec<&str>) {
+    for line in result {
+        println!("{}", line);
+    }
+}
+
+pub fn analyze_file<'a>(cfg: &'a Config, content: &'a String) -> Vec<&'a str> {
+    let mut result: Vec<&str> = Vec::new();
+    for line in content.lines() {
+        if line.contains(&cfg.query) {
+            result.push(line);
+        }
+    }
+
+    result
 }
--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,51 +1,36 @@
 extern crate argparse;
 
-use argparse::{ArgumentParser, Store};
+use argparse::{ ArgumentParser, Store };
 use std::fs;
 use std::env;
-use minigrep::Config;
+use minigrep::*;
 
 fn main() {
-
     let mut filename: String = "".to_string();
     let mut query: String = "".to_string();
-    {  // this block limits scope of borrows by ap.refer() method
+    {
+        // this block limits scope of borrows by ap.refer() method
         let mut ap: ArgumentParser = ArgumentParser::new();
         ap.set_description("Minigrep app emulates a grep command but in a more simple way");
-        ap.refer(&mut filename)
-            .add_argument("file", Store,
-            "File to open").required();
-        ap.refer(&mut query)
-            .add_argument("query", Store,
-            "String to search").required();
+        ap.refer(&mut filename).add_argument("file", Store, "File to open").required();
+        ap.refer(&mut query).add_argument("query", Store, "String to search").required();
         ap.parse_args_or_exit();
     }
 
-    let cfg: Config = Config {filename, query};
+    let cfg: Config = Config { filename, query };
 
     println!("File: {}", cfg.filename);
     println!("Query: {}", cfg.query);
 
     match env::current_exe() {
-        Ok(exe_path) => println!("Path of this executable is: {}",
-                                exe_path.display()),
+        Ok(exe_path) => println!("Path of this executable is: {}", exe_path.display()),
         Err(e) => println!("failed to get current exe path: {e}"),
-    };
-
-    let content: String = fs::read_to_string(&cfg.filename).expect(&format!("Error reading the file {}", cfg.filename)[..]);
-
-    let mut result: Vec<&str> = Vec::new();
-    for line in content.lines()
-    {
-        if line.contains(&cfg.query){
-            result.push(line)
-        }
     }
 
+    let content = fs
+        ::read_to_string(&cfg.filename)
+        .expect(&format!("Error reading the file {}", cfg.filename)[..]);
+    let result: Vec<&str> = analyze_file(&cfg, &content);
 
-    for line in result{
-        println!("{}", line);
-    }
-   
+    print_result(&result);
 }
-
